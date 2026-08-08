d0bde5609bd30eb2d05e6fdd5681ed13e811c466
nakagawach <nakagawach@gmail.com>
2026-08-08 21:32:58 +0900

    webhook再設定２

--- src/App.tsx
+++ src/App.tsx
@@ -215,7 +215,7 @@ async function uploadPhotoToMake(
   if (
     !MAKE_WEBHOOK_URL ||
     MAKE_WEBHOOK_URL ===
-      "https://hook.us1.make.com/t4kttpgnjopclycliqczbyaqe1qb467vL"
+      "https://hook.us1.make.com/t4kttpgnjopclycliqczbyaqe1qb467v"
   ) {
     throw new Error(
       "Make Webhook URLをApp.tsxへ設定してください。"
